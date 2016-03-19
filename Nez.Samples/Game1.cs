a1893f9aaa61febaae16486aa16b645abf3876e9
Mike <mike@prime31.com>
2016-03-19 13:37:28 -0700

    initial commit

---
+++ Nez.Samples/Game1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Nez.Samples
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager( this );
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+
+		protected override void Update( GameTime gameTime )
+		{
+			#if !__IOS__ &&  !__TVOS__
+			if( GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown( Keys.Escape ) )
+				Exit();
+			#endif
+
+			base.Update( gameTime );
+		}
+
+
+		protected override void Draw( GameTime gameTime )
+		{
+			graphics.GraphicsDevice.Clear( Color.CornflowerBlue );
+            
+		
+            
+			base.Draw( gameTime );
+		}
+	}
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Nez-Samples
 Samples and demos of various Nez features
+
+
+Setup
+----
+Pull a fresh copy of the Nez-Samples repository. The samples repository has the base Nez repository as a submodule so to fully download everything you need add the `--recursive` paramter when cloning:
+
+`git clone --recursive https://github.com/prime31/Nez-Samples.git`
+
+Visual Studio/Xamarin *should* download the NuGet packages for you when opening the solution. There appears to be a bug with Xamarin where it will not properly download NuGet packages. If this happens open the Nez.sln and it should update the packages. You can then close it and reopen the Nez.Samples.sln and it should now compile.
+
+
