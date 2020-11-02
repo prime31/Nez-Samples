9d7635b4b7cb15192be6663094f60783f09c6efd
bvs3 <bvansickle3@gmail.com>
2020-11-02 09:28:21 -0500

    Simple demo scene showing water reflection effect issue.  Works on Monogame, broken for FNA (specifically FNA3D)

---
+++ Nez.Samples/Scenes/WaterPlane/WaterPlaneScene.cs
@@ -0,0 +1,52 @@
+﻿using Nez.Sprites;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Nez.Samples
+{
+	/// <summary>
+	/// Tiled map import that includes animated tiles from multiple different tileset images
+	/// </summary>
+	[SampleScene("Water Reflection Effect", 70, "Water Reflection Effect")]
+	public class WaterPlaneScene : SampleScene
+	{
+		const int _waterPaneRenderLayer = -50;
+
+		public WaterPlaneScene() : base(false, true)
+		{
+			AddRenderer(new RenderLayerExcludeRenderer(0, new[] { _waterPaneRenderLayer, ScreenSpaceRenderLayer }));
+
+			var waterrenderer = new RenderLayerRenderer(-1, _waterPaneRenderLayer);
+			waterrenderer.WantsToRenderAfterPostProcessors = true;
+			AddRenderer(waterrenderer);
+
+		}
+
+
+		public override void Initialize()
+		{
+			AddRenderer(new RenderLayerExcludeRenderer(0, new[] { _waterPaneRenderLayer, ScreenSpaceRenderLayer }));
+
+			var waterrenderer = new RenderLayerRenderer(-1, _waterPaneRenderLayer);
+			waterrenderer.WantsToRenderAfterPostProcessors = true;
+			AddRenderer(waterrenderer);
+		}
+
+		public override void OnStart()
+		{
+
+			var moonTex = Content.Load<Texture2D>(Nez.Content.Shared.Moon);
+			var playerEntity = CreateEntity("player", new Vector2(Screen.Width / 2, Screen.Height * .7f));
+			playerEntity.AddComponent(new SpriteRenderer(moonTex));
+
+			var reflectiveEntity = Core.Scene.CreateEntity("Reflection");
+			var reflectionPlane = new WaterReflectionPlane(Screen.Width, Screen.Height / 5) { RenderLayer = 3 };
+			var material = reflectionPlane.GetMaterial<WaterReflectionMaterial>();
+			material.Effect.NormalMap = Core.Scene.Content.LoadTexture("Content/WaterPane/waterNormalMap.png");
+			material.Effect.PerspectiveCorrectionIntensity = .01f;
+			material.Effect.NormalMagnitude = 0.01f;
+			reflectiveEntity.AddComponent(reflectionPlane);
+			reflectiveEntity.Position = new Vector2(0, Screen.Height - Screen.Height / 5);
+		}
+	}
+}
